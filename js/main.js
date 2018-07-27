a6756979893c14c09199c0fe8f4f556bf2f0f515
mozukraken <mozukraken@gmail.com>
2018-07-28 07:56:08 +0900

    行要素を取得した。

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@
 
   for (i = 0; i < ths.length; i++) {
     ths[i].addEventListener('click', function() {
-      console.log(this.cellIndex);
+    //   console.log(this.cellIndex);
+
+    // NodeList
+    // var rows = document.querySelectorAll('tbody > tr');
+    var rows = Array.prototype.slice.call(document.querySelectorAll('tbody > tr'));
+    console.log(rows);
+    return;
     });
   }
 
